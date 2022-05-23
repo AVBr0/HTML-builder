25828f73d1e92c35c9d183c6e2728137eccc3c3a
avbr0 <logweb01@gmail.com>
2022-05-24 01:04:43 +0300

    fix: uptade files-copy 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,12 +1,13 @@
 const path = require('path');
-const {mkdir, readdir, copyFile} = require('fs/promises');
+const {mkdir, readdir, copyFile, rm} = require('fs/promises');
 
 const originalFolderPath = path.join(__dirname, 'files');
 const newFolderPath = path.join(__dirname, 'files-copy');
 
 (async () => {
   try {
-    await mkdir(newFolderPath, {recursive:true});
+    await rm(newFolderPath, { recursive: true, force: true });
+    await mkdir(newFolderPath, { recursive: true });
     const originalFiles = await readdir(originalFolderPath, {withFileTypes:true});
     originalFiles.forEach(f => {
       if(f.isFile) {
